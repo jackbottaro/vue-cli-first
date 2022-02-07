371aa66c7e4974b2cf46c550420e322fe3622e66
jackbottaro <mail.giacomobottaro@gmail.com>
2022-02-07 17:54:52 +0100

    added jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <h1>La mia prima app con Vue CLI!</h1>
+
+    <Jumbotron />
+
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+
+<script>
+import Jumbotron from "./components/Jumbotron.vue";
 export default {
   name: "App",
+  components : {
+    Jumbotron: Jumbotron,
+  }
 };
 </script>
 
@@ -20,8 +27,10 @@ export default {
 
 h1 {
   margin-top: 50px;
+  font-family: 'Permanent Marker', cursive;
 
 }
 
+@import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
 
 </style>
